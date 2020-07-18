6961bfdd8990bacc54c0b861fd67a247eda4c942
sadisticsolution <sadisticsolutione@gmail.com>
2020-07-18 02:13:30 -0700

    Attempt to fix css zoom bug

---
+++ build/precache-manifest.50a7773231fedd1484603cfebc54ec75.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3fe38b85c18bf6f90a2e19bf3be2762",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9f73ab7490d127b73b23",
+    "url": "./static/css/2.85f22f3a.chunk.css"
+  },
+  {
+    "revision": "d10a5d579d0786831d5e",
+    "url": "./static/css/main.64d65334.chunk.css"
+  },
+  {
+    "revision": "9f73ab7490d127b73b23",
+    "url": "./static/js/2.b1953735.chunk.js"
+  },
+  {
+    "revision": "17c3c6f91729e62e1ce23162d119031b",
+    "url": "./static/js/2.b1953735.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d10a5d579d0786831d5e",
+    "url": "./static/js/main.351e5046.chunk.js"
+  },
+  {
+    "revision": "dfec98373cde2379d13a",
+    "url": "./static/js/runtime-main.c03e894b.js"
+  }
+]);
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.6ff6c8543b55af6ff0f700ffd89fcb74.js"
+  "./precache-manifest.50a7773231fedd1484603cfebc54ec75.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
